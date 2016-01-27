a915eda24c84b576906b1289e1d5ce977f4bf1cd
Miriam Lee <mimibaari+githubmimiAsusIII@gmail.com>
2016-01-26 23:41:48 -0600

    Week 1 Percolation Stats with mean, stddev and confidence
    Compilation:  PASSED
    Style:        FAILED
    Findbugs:     Potential bugs found.
    API:          PASSED
    
    Correctness:  14/26 tests passed
    Memory:       8/8 tests passed
    Timing:       9/9 tests passed
    
    Aggregate score: 70.00% [Correctness: 65%, Memory: 10%, Timing: 25%, Style: 0%]

--- Percolation/src/PercolationStats.java
+++ Percolation/src/PercolationStats.java
@@ -1,9 +1,20 @@
+
 /**
- * @author mimi
- *
- *
+ * @author Miriam Lee
+ * @date
+ * @purpose
+ * @howto
  */
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
 public class PercolationStats {
+  private double[] x;
+  private double T;
+  // private int N;
+
   /**
    * perform T independent experiments on an N-by-N grid
    * 
@@ -12,7 +23,43 @@ public class PercolationStats {
    * @throws java.lang.IllegalArgumentException
    *           if either N ≤ 0 or T ≤ 0
    */
-  public PercolationStats(int N, int T) throws java.lang.IllegalArgumentException {
+  public PercolationStats(int N, int T) throws IllegalArgumentException {
+
+    if (N <= 0 || T <= 0)
+      throw new IllegalArgumentException();
+
+    this.T = T;
+    // this.N = N;
+    x = new double[T];
+    Percolation p;
+
+    // performs T independent computational experiments (discussed above) on
+    // an N-by-N grid
+    int i, j, opened;
+
+    while (T-- > 0) {
+      p = new Percolation(N);
+      opened = 0;
+      // Repeat the following until the system percolates:
+      while (opened < (N * N) && !p.percolates()) {
+
+        do { // Choose a site (row i, column j) uniformly at random among all
+             // blocked sites.
+          i = StdRandom.uniform(1, N + 1);
+          j = StdRandom.uniform(1, N + 1);
+          // System.out.println(i + " " + j);
+        } while (p.isOpen(i, j));
+
+        // Open the site (row i, column j).
+        p.open(i, j);
+        opened++;
+        // PercolationVisualizer.draw(p, N);
+        // StdDraw.show(0);
+      }
+      // The fraction of sites that are opened when the system percolates
+      // provides an estimate of the percolation threshold.
+      x[T] = (double) opened / (N * N);
+    }
   }
 
   /**
@@ -21,7 +68,7 @@ public class PercolationStats {
    * @return
    */
   public double mean() {
-    return 0;
+    return StdStats.mean(x);
   }
 
   /**
@@ -30,7 +77,7 @@ public class PercolationStats {
    * @return
    */
   public double stddev() {
-    return 0;
+    return StdStats.stddev(x);
   }
 
   /**
@@ -39,8 +86,8 @@ public class PercolationStats {
    * @return
    */
   public double confidenceLo() {
-    return 0;
-  } //
+    return mean() - (1.96 * stddev()) / Math.sqrt(T);
+  }
 
   /**
    * high endpoint of 95% confidence interval
@@ -48,7 +95,7 @@ public class PercolationStats {
    * @return
    */
   public double confidenceHi() {
-    return 0;
+    return mean() + (1.96 * stddev()) / Math.sqrt(T);
   } //
 
   /**
@@ -57,7 +104,18 @@ public class PercolationStats {
    * @param args
    */
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
+    // turn on animation mode
+    // StdDraw.show(0);
+
+    // takes two command-line arguments N and T,
+    PercolationStats ps = new PercolationStats((int) Integer.parseInt(args[0]),
+        (int) Integer.parseInt(args[1]));
+
+    // prints the mean, standard deviation, and the 95% confidence interval for
+    // the percolation threshold.
+    StdOut.printf("mean                    = %.16f\n", ps.mean());
+    StdOut.printf("stddev                  = %.17f\n", ps.stddev());
+    StdOut.printf("95%% confidence interval = %.16f, %.16f\n", ps.confidenceLo(), ps.confidenceHi());
 
   }
 
