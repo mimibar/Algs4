44e212fdebc5cbb048fdd25bcde028a85f082784
Miriam Lee <mimibaari+githubmimiAsusIII@gmail.com>
2016-01-26 21:09:06 -0600

    Week 1 Percolation (with backwash)

---
+++ Percolation/src/Percolation.java
@@ -0,0 +1,153 @@
+
+/**
+ * @author Miriam Lee
+ * @date
+ * @purpose 
+ * @howto
+ */
+
+import java.util.Arrays;
+
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+  private final int N;
+  private boolean[][] open;
+  private WeightedQuickUnionUF uf;
+
+  /**
+   * create N-by- N grid, with all sites blocked
+   * 
+   * @param N
+   * @throws java.lang.IllegalArgumentException
+   *           if N ≤ 0
+   */
+  public Percolation(int N) throws IllegalArgumentException {
+    if (N <= 0)
+      throw new IllegalArgumentException();
+
+    this.N = N;
+
+    open = new boolean[N + 2][N + 2];
+    Arrays.fill(open[0], true); // top open
+    Arrays.fill(open[N + 1], true); // bottom open
+
+    // which sites are connected to which other sites. The last of these is
+    // exactly what the union-find data structure is designed for.
+    this.uf = new WeightedQuickUnionUF((N+2) * (N + 2)); // plus two extra rows
+                                                       // (bottom + top)
+
+    for (int i = 0; i <= N; i++) { // top
+//      if (i < N)
+        uf.union(1, i + 1);
+//      uf.union(i, N + i);
+    }
+    for (int i = 0; i <= N; i++) { // bottom
+//      if (i < N)
+        uf.union(xyTo1D(N + 1, i), xyTo1D(N + 1, i + 1));
+//      uf.union(xyTo1D(N + 1, i), xyTo1D(N, i));
+    }
+  }
+
+  /**
+   * mapping 2D coordinates to 1D coordinates.
+   * 
+   * Map from a 2-dimensional (row, column) pair (Base 1) to a 1-dimensional
+   * union find object index (Base 1)
+   * 
+   * @param x
+   * @param y
+   * @return
+   */
+  private int xyTo1D(int x, int y) {
+    return x * (N) + y;
+  }
+
+  private boolean validateIndices(int i, int j) throws IndexOutOfBoundsException {
+    if (i <= 0 || i > N)
+      throw new IndexOutOfBoundsException("row index i out of bounds");
+    if (j <= 0 || j > N)
+      throw new IndexOutOfBoundsException("column index j out of bounds");
+
+    return true;
+  }
+
+  /**
+   * open site (row i, column j) if it is not open already
+   * 
+   * @param i
+   * @param j
+   * @throws java.lang.IndexOutOfBoundsException
+   */
+  public void open(int i, int j) throws IndexOutOfBoundsException {
+    // First, it should validate the indices of the site that it receives.
+    if (validateIndices(i, j)) {
+
+      // Second, it should somehow mark the site as open.
+      open[i][j] = true;
+      
+      // Third, it should perform some sequence of WeightedQuickUnionUF
+      // operations that links the
+      // site in question to its open neighbors.
+      if (i + 1 <= N+1 && open[i + 1][j]) {
+//        if (i+1 != N+1 || uf.connected(1, xyTo1D(i, j))) //FOR BackWash
+          uf.union(xyTo1D(i, j), xyTo1D(i + 1, j));
+      }
+      if (j + 1 <= N && open[i][j + 1])
+        uf.union(xyTo1D(i, j), xyTo1D(i, j + 1));
+
+      if (i - 1 >= 0 && open[i - 1][j])
+        uf.union(xyTo1D(i, j), xyTo1D(i - 1, j));
+
+      if (j - 1 > 0 && open[i][j - 1])
+        uf.union(xyTo1D(i, j), xyTo1D(i, j - 1));
+    }
+  }
+
+  /**
+   * is site (row i, column j) open?
+   * 
+   * @param i
+   * @param j
+   * @return
+   * @throws java.lang.IndexOutOfBoundsException
+   */
+  public boolean isOpen(int i, int j) throws IndexOutOfBoundsException {
+    return open[i][j];
+  }
+
+  /**
+   * is site (row i, column j) full?
+   * 
+   * A full site is an open site that can be connected to an open site in the
+   * top row via a chain of neighboring (left, right, up, down) open sites.
+   * 
+   * @param i
+   * @param j
+   * @return
+   * @throws java.lang.IndexOutOfBoundsException
+   */
+  public boolean isFull(int i, int j) throws IndexOutOfBoundsException {
+    return uf.connected(1, xyTo1D(i, j));
+  }
+
+  /**
+   * does the system percolate?
+   * 
+   * @return
+   */
+  public boolean percolates() {
+    return uf.connected(1, (N+1) * (N+1)-1 ); // xyTo1D(N+1, N));
+  }
+
+  /**
+   * test client (optional)
+   * 
+   * @param args
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
---
+++ Percolation/src/PercolationStats.java
@@ -0,0 +1,64 @@
+/**
+ * @author mimi
+ *
+ *
+ */
+public class PercolationStats {
+  /**
+   * perform T independent experiments on an N-by-N grid
+   * 
+   * @param N
+   * @param T
+   * @throws java.lang.IllegalArgumentException
+   *           if either N ≤ 0 or T ≤ 0
+   */
+  public PercolationStats(int N, int T) throws java.lang.IllegalArgumentException {
+  }
+
+  /**
+   * sample mean of percolation threshold
+   * 
+   * @return
+   */
+  public double mean() {
+    return 0;
+  }
+
+  /**
+   * sample standard deviation of percolation threshold
+   * 
+   * @return
+   */
+  public double stddev() {
+    return 0;
+  }
+
+  /**
+   * low endpoint of 95% confidence interval
+   * 
+   * @return
+   */
+  public double confidenceLo() {
+    return 0;
+  } //
+
+  /**
+   * high endpoint of 95% confidence interval
+   * 
+   * @return
+   */
+  public double confidenceHi() {
+    return 0;
+  } //
+
+  /**
+   * test client (described below)
+   * 
+   * @param args
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
