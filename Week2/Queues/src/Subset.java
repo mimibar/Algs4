06840d41e0cc9a4675f5bf0d2bd2cba2af359040
Miriam Lee <mimibaari+githubmimiAsusIII@gmail.com>
2016-02-07 00:01:27 -0600

    [draft] Week 2 Programming Assignment 2: Randomized Queues and Deques Attempt at bonus...

--- Week2/Queues/src/Subset.java
+++ Week2/Queues/src/Subset.java
@@ -6,6 +6,7 @@
  */
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
 
 /**
  * Takes a command-line integer k; reads in a sequence of N strings from
@@ -21,14 +22,26 @@ public class Subset {
    */
   public static void main(String[] args) {
     int k = Integer.parseInt(args[0]);
-    int N = 0;
+    boolean[] kb = new boolean[N];
 
+    int kk = k, p;
+    while (kk-- > 0) {
+      do {
+        p = StdRandom.uniform(N);
+      } while (kb[p]);
+      kb[p] = true;
+      kk--;
+    }
+
+    int N = 0;
     RandomizedQueue<String> s = new RandomizedQueue<String>();
+
     while (!StdIn.isEmpty()) {
       String item = StdIn.readString();
-      s.enqueue(item);
-//      StdOut.println(item);
-        N++;
+      if (kb[N])
+        s.enqueue(item);
+      // StdOut.println(item);
+      N++;
     }
     while (k-- > 0) {
       StdOut.println(s.dequeue());
