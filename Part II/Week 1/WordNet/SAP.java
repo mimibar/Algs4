332a112bfb4bef5d7819ccb0b2854ae9f2d2baad
Miriam Lee <mimibaari+githubmimiAsusIII@gmail.com>
2016-04-07 20:28:30 -0600

    [draft] Part II Week 1 Programming Assignment 1: WordNet
    SAP - straightforward, run two breadth-first searchesn, for each synset reachable from x and from y, sum the two distances, take the
    minimum of these sums.

--- Part II/Week 1/WordNet/SAP.java
+++ Part II/Week 1/WordNet/SAP.java
@@ -5,6 +5,7 @@
  * @howto
  */
 
+import edu.princeton.cs.algs4.BreadthFirstDirectedPaths;
 import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdIn;
@@ -23,13 +24,42 @@ import edu.princeton.cs.algs4.StdOut;
  */
 public class SAP {
 
+  private Digraph dg;
+  private Integer[][] len;
+  private Integer[][] anc;
+
   /**
-   * constructor takes a digraph (not necessarily a DAG)
+   * constructor takes a digraph (not necessarily a DAG).</br>
+   *
+   * 'The most straightforward way to find a shortest ancestral path is to run
+   * two breadth-first searches in G starting from x and from y, thereby
+   * computing the distance of each synset from x and from y. Then, for each
+   * synset reachable from x and from y, sum the two distances, Finally, take
+   * the minimum of these sums and subtract two. (Or, instead of subtracting
+   * two, in each distance computation avoid counting the first edge.) An
+   * improvement is to run both searches concurrently, visiting vertices at
+   * distance one from either x or y, then vertices at distance two from x or y,
+   * and so on. Once some vertex is reached from both x and y, the sum of its
+   * distances from x and y gives an upper bound on the distance beyond which
+   * the searches need not go. The upper bound can be reduced each time a
+   * shorter pair of paths is found. This allows early termination, especially
+   * in the case of nearby words. </br>
+   * If W is acyclic, an additional heuristic can be used to try to further
+   * limit the searches ... Do a preprocessing step to number the vertices in
+   * topological order. When choosing whether to do a step of the BFS from x or
+   * the one from y, visit the vertex smaller in topological order. This
+   * strategy can be combined with the shortest distance strategy in various
+   * ways.' <a href=
+   * "http://www.cs.princeton.edu/courses/archive/fall10/cos226/precepts/WordNet-Tarjan.pdf">
+   * WordNet-Tarjan.pdf</a>
    *
    * @param G
    *          a digraph (not necessarily a DAG)
    */
   public SAP(Digraph G) {
+    dg = new Digraph(G);
+    len = new Integer[dg.V()][dg.V()];
+    anc = new Integer[dg.V()][dg.V()];
   }
 
   /**
@@ -44,6 +74,36 @@ public class SAP {
    *           if any argument vertex is invalid—not between 0 and G.V() - 1.
    */
   public int length(int v, int w) {
+    isNotNull(v);
+    isNotNull(w);
+    isVertexValid(v);
+    isVertexValid(w);
+
+    if (len[v][w] != null)// use cached length
+      return len[v][w];
+
+    BreadthFirstDirectedPaths bfDpV = new BreadthFirstDirectedPaths(dg, v);
+    BreadthFirstDirectedPaths bfDpW = new BreadthFirstDirectedPaths(dg, w);
+
+    int min = Integer.MAX_VALUE, ancestor = -1;
+    for (int i = 0; i < dg.V(); i++) {
+      if (i == v)
+        continue;
+
+      if (bfDpV.hasPathTo(i) && bfDpW.hasPathTo(i)
+          && bfDpV.distTo(i) + bfDpW.distTo(i) < min) {
+        min = bfDpV.distTo(i) + bfDpW.distTo(i);
+        ancestor = i;
+      }
+
+    }
+    if (min == Integer.MAX_VALUE)
+      min = -1;
+
+    len[v][w] = min;
+    anc[v][w] = ancestor;
+
+    return min;
   }
 
   /**
@@ -60,6 +120,15 @@ public class SAP {
    *           if any argument vertex is invalid—not between 0 and G.V() - 1.
    */
   public int ancestor(int v, int w) {
+    isNotNull(v);
+    isNotNull(w);
+    isVertexValid(v);
+    isVertexValid(w);
+
+    if (len[v][w] == null)
+      length(v, w);
+
+    return anc[v][w];
   }
 
   /**
@@ -76,6 +145,53 @@ public class SAP {
    *           if any argument vertex is invalid—not between 0 and G.V() - 1.
    */
   public int length(Iterable<Integer> v, Iterable<Integer> w) {
+    isNotNull(v);
+    isNotNull(w);
+
+    int min = Integer.MAX_VALUE;
+
+    for (int i : v) {
+      isVertexValid(i);
+      for (int j : w) {
+        isVertexValid(j);
+
+        if (len[i][i] == null) {
+          len[i][i] = length(i, j);
+
+          if (len[i][i] > -1 && len[i][i] < min)
+            min = len[i][i];
+        }
+      }
+    }
+    if (min < Integer.MAX_VALUE)
+      min -= 0;
+    else
+      min = -1;
+    return min;
+  }
+
+  /**
+   * @param o
+   * @return true if argument not null
+   * @throws NullPointerException
+   *           if any argument is null.
+   */
+  private boolean isNotNull(Object o) {
+    if (o == null)
+      throw new NullPointerException();
+    return true;
+  }
+
+  /**
+   * @param v
+   * @return true if vertex is between 0 and G.V() - 1
+   * @throws IndexOutOfBoundsException
+   *           if any argument vertex is invalid—not between 0 and G.V() - 1.
+   */
+  private boolean isVertexValid(int v) {
+    if (v < 0 || v > dg.V() - 1)
+      throw new IndexOutOfBoundsException();
+    return true;
   }
 
   /**
@@ -91,6 +207,27 @@ public class SAP {
    *           if any argument vertex is invalid—not between 0 and G.V() - 1.
    */
   public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
+
+    isNotNull(v);
+    isNotNull(w);
+    int min = Integer.MAX_VALUE, ancestor = -1;
+
+    for (int i : v) {
+      isVertexValid(i);
+      for (int j : w) {
+        isVertexValid(j);
+
+        if (len[i][j] == null)
+          length(i, j);
+
+        if (len[i][j] > -1 && len[i][j] < min) {
+          min = len[i][j];
+          ancestor = anc[i][j];
+        }
+      }
+    }
+
+    return ancestor;
   }
 
   /**
